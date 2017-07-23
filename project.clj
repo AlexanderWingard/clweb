738a78ddda5bbc1c714ed13cfd25f1ac2d5e75a6
Alexander Wingård <alexander.wingard@gmail.com>
2017-07-24 01:50:42 +0200

    Pull in reagent and forms module

--- project.clj
+++ project.clj
@@ -1,15 +1,17 @@
 (defproject clweb "0.1.0-SNAPSHOT"
   :dependencies [
-                 [org.clojure/clojure "1.8.0"]
-                 [com.cemerick/piggieback "0.2.1"]
-                 [org.clojure/clojurescript "1.9.229"]
-                 [compojure "1.5.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]
-                 [http-kit "2.1.19"]
                  [cljsjs/d3 "4.3.0-4"]
                  [cljsjs/semantic-ui "2.2.4-0"]
+                 [com.cemerick/piggieback "0.2.1"]
+                 [compojure "1.5.0"]
                  [figwheel-sidecar "0.5.0-2"]
+                 [http-kit "2.1.19"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [reagent "0.7.0"]
+                 [reagent-forms "0.5.29"]
                  [ring-cljsjs "0.1.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
                  ]
   :plugins [
             [lein-figwheel "0.5.9"]
--- src/clweb/core.cljs
+++ src/clweb/core.cljs
@@ -1,61 +1,56 @@
 (ns clweb.core
-  (:require [cljs.reader :as reader]
-            [clweb.types :as t]
-            [clojure.string :as str]
-            [cljsjs.d3 :as d3]
-            [cljsjs.semantic-ui]
-            [cljs.test]
-            ))
-(reader/register-tag-parser! "clweb.types.Greeting" t/map->Greeting)
-
-(defonce state (atom {:list []}))
-
+  (:require
+   [cljs.reader :as reader]
+   [cljs.test]
+   [cljsjs.d3 :as d3]
+   [cljsjs.semantic-ui]
+   [clojure.string :as str]
+   [clweb.types :as t]
+   [reagent-forms.core :refer [bind-fields]]
+   [reagent.core :as reagent]
+   ))
+(defonce state (atom {}))
 (enable-console-print!)
+
 (def ws-uri
   (let [location (-> js/window .-location)
         host (-> location .-host)
         protocol (-> location .-protocol (case "http:" "ws:" "https:" "wss:"))]
     (str protocol "//" host "/ws")))
 (def ws (js/WebSocket. ws-uri))
-(defn handle [ws-event]
-  (let [greet (-> ws-event
-                  (.-data)
-                  (reader/read-string)
-                  (t/greeting))]
-    (swap! state update-in [:list] #(conj % greet))))
-(aset ws "onmessage" handle)
-(aset ws "onopen" (fn [] (.send ws (pr-str (t/Greeting. "I'm here")))))
+(defn ws-message [ws-event]
+  ())
+(defn ws-open []
+  ())
+(aset ws "onmessage" ws-message)
+(aset ws "onopen" ws-open)
 (aset js/window "onhashchange" (fn [] (println (apply hash-map (str/split (subs (.-hash (.-location js/window)) 1) #"/")))))
-(def in (.getElementById js/document "in"))
-(defn render [parent term]
-  (if (coll? term)
-    (let [select (.. parent
-                     (selectAll (fn []  (.-childNodes (.node parent))))
-                     (data (apply array term)))
-          enter  (.. select
-                     (enter)
-                     (append "div"))
-          exit   (.. select
-                     (exit)
-                     (remove))
-          update (.. select
-                     (merge enter)
-                     (style "background-color" "rgba(1, 1, 1, 0.2)")
-                     (style "margin-left" "20px")
-                     (each (fn [d] (this-as t (render (.select js/d3 t) d)))))])
-    (.text parent term)))
 
-(defn parse-in []
-  (let [term (try
-               (reader/read-string (.-value in))
-               (catch js/Error e '(nil)))
-        top (.select js/d3 "#out")]
-    (render top term))
-  )
-(aset in "onkeyup" parse-in)
-(parse-in)
+(defn input [label type id]
+  [:input.form-control {:field type :id id :placeholder label}])
 
-(defn the-truth [] true)
+(def form-template
+  [:div.ui.segment
+   [:div.ui.form
+    [:div.ui.field
+     [:label "Username:"]
+     [:div.ui.icon.input
+      (input "Username" :text :user.name)
+      [:i.user.icon]]]
+    [:div.ui.field
+     [:label "Password:"]
+     [:div.ui.icon.input
+      (input "Password" :password :user.password)
+      [:i.lock.icon]]]
+    [:button.ui.button "Login"]]])
 
-(defn figwheel-reload []
-  (println @state))
+(defn app []
+  (let [s (reagent/atom {})]
+    (fn []
+      [:div.ui.container
+       [bind-fields form-template s]
+       [:div (str @s)]])))
+
+(reagent/render [app] (js/document.getElementById "app"))
+
+(defn figwheel-reload [])
