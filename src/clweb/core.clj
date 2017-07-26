fc896339c44d0012a1c73c9824b7ca8be52ac672
Alexander Wingård <alexander.wingard@gmail.com>
2017-07-26 22:22:31 +0200

    Enable login and out

--- src/clweb/core.clj
+++ src/clweb/core.clj
@@ -17,15 +17,22 @@
 
 (add-watch clients :watcher
            (fn [key atom old-state new-state]
-             (publish-to-all (prn-str {:action "server-state" :state new-state}))))
+             (publish-to-all (prn-str {:action "full-server-state" :state new-state}))))
+
+(defn handle-msg [channel string]
+  (let [data (edn/read-string string)]
+    (case (:action data)
+      "login" (let [newstate (swap! clients assoc-in [channel :logged-in] true)]
+                (send! channel (pr-str {:action "your-state" :state (get newstate channel)})))
+      "logout" (let [newstate (swap! clients assoc-in [channel :logged-in] false)]
+                 (send! channel (pr-str {:action "your-state" :state (get newstate channel)}))))))
+
 
 (defn ws-handler [req]
   (with-channel req channel
     (swap! clients assoc channel {})
     (on-close channel (fn [status] (swap! clients dissoc channel)))
-    (on-receive channel (fn [string] (let [data (edn/read-string string)]
-                                       (case (:action data)
-                                         "login" (swap! clients update-in [channel :logged-in] not)))))))
+    (on-receive channel (fn [message] (#'handle-msg channel message)))))
 
 (defroutes routes
   (GET "/" [] (resource-response "index.html" {:root "public"}))
--- src/clweb/core.cljs
+++ src/clweb/core.cljs
@@ -12,8 +12,9 @@
    [reagent-forms.core :refer [bind-fields]]
    [reagent.core :as reagent :refer [atom]]
    ))
-(defonce state (atom {}))
+(defonce client-state (atom {}))
 (defonce server-state (atom {}))
+(defonce full-server-state (atom {}))
 (enable-console-print!)
 (register-tag-parser! "object" (fn [arg] (prn-str arg)))
 
@@ -26,8 +27,11 @@
 (defn ws-on-message [ws-event]
   (let [message (reader/read-string (.-data  ws-event))]
     (case (:action message)
-      "server-state" (reset! server-state (:state message)))))
-(defn ws-open []
+      "full-server-state" (reset! full-server-state (:state message))
+      "your-state" (swap! server-state merge (:state message)))))
+
+(defn ws-open [
+               ]
   ())
 (defn ws-send [data]
   (.send ws (pr-str data)))
@@ -51,23 +55,33 @@
     [:div.ui.icon.input
      (input "Password" :password :user.password)
      [:i.lock.icon]]]
-   [:button.ui.button {:on-click #(ws-send (assoc (:user @state) :action "login"))} "Login"]])
+   [:button.ui.button {:on-click #(ws-send (assoc (:user @client-state) :action "login"))} "Login"]])
 
 (defn render-clojure [atom]
   (prewalk-replace
    {:table.jh-type-object :table.jh-type-object.ui.celled.table}
    (json-html.core/edn->hiccup @atom)))
 
+
+(defn login []
+  [:div.ui.segment
+   [:div.ui.form
+    [bind-fields form-template client-state]]])
+(defn logout []
+  [:button.ui.button {:on-click #(ws-send {:action "logout"})} "Logout"])
+
 (defn app []
   [:div.ui.container
    [:h1.ui.header "Charlies Bank"]
-   [:div.ui.segment
-    [:div.ui.form
-     [bind-fields form-template state]]]
+   (if (not (:logged-in @server-state))
+     [login]
+     [logout])
    [:h1.ui.header "Client state"]
-   (render-clojure state)
+   (render-clojure client-state)
    [:h1.ui.header "Server state"]
-   (render-clojure server-state)])
+   (render-clojure server-state)
+   [:h1.ui.header "Full Server state"]
+   (render-clojure full-server-state)])
 
 (reagent/render [app] (js/document.getElementById "app"))
 
