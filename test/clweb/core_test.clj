3197ee064cc29391ac303abc4623efa88e255c6b
Alexander Wingård <alexander.wingard@gmail.com>
2017-07-27 04:45:28 +0200

    A failing testcase

--- test/clweb/core_test.clj
+++ test/clweb/core_test.clj
@@ -1,7 +1,10 @@
 (ns clweb.core-test
   (:require [clojure.test :refer :all]
-            [clweb.core :refer :all]))
+            [clweb.core :refer :all]
+            [org.httpkit.server :as httpkit]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "FXME, I fail."
+    (is (= nil
+           (with-redefs [httpkit/send! (fn [_ message] message)]
+             (handle-msg nil (prn-str {:action "logout"})))))))
