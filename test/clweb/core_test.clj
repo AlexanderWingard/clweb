b0f0f85acc02955bf604391330241fb9783d19c9
Alexander Wingård <alexander.wingard@gmail.com>
2017-08-07 01:48:54 +0200

    Barf some tests

--- test/clweb/core_test.clj
+++ test/clweb/core_test.clj
@@ -37,25 +37,32 @@
                  {:a {:value "val" :error "err"}
                   :b {:value "val" :error "err b"}}}]
       (is (any-errors? state :id))
-      (is (not (any-errors? (clear-errors state :id) :id)))))
+      (is (not (any-errors? (clear-errors state :id) :id))))))
 
-  (deftest login-test
-    (testing "new logintest"
-      (let [fe-state (-> {}
-                         (assoc-val login-form/username-path "alexander")
-                         (assoc-val login-form/password-path "password")
-                         (atom))
-            be-state (->  (state/new)
-                          (state/register-user "alexander" "password"))
-            request (login-form/on-click-login nil fe-state)
-            response (be-action nil request be-state)]
-        (fe-action nil response fe-state)
-        (is (= login-form/login-successful (:action response)))
-        (is (nil? (login-form/state-key @fe-state)) "Login form is cleared")
-        (is (= "alexander" (state/logged-in-user be-state nil)))
-        (be-action nil (login-form/on-click-logout nil fe-state) be-state)
-        (is (= nil (login-form/logged-in-key @fe-state)))
-        (is (= nil (state/logged-in-user be-state nil)))))))
+(deftest ws-send-test
+  (testing "Checking sent msgs"
+    (let [msgs (atom [])]
+      (ws-send msgs "test1")
+      (ws-send msgs "test2")
+      (is (= ["test1" "test2"] @msgs)))))
+
+(deftest login-test
+  (testing "new logintest"
+    (let [fe-state (-> {}
+                       (assoc-val login-form/username-path "alexander")
+                       (assoc-val login-form/password-path "password")
+                       (atom))
+          be-state (->  (state/new)
+                        (state/register-user "alexander" "password"))
+          request (login-form/on-click-login nil fe-state)
+          response (be-action nil request be-state)]
+      (fe-action nil response fe-state)
+      (is (= login-form/login-successful (:action response)))
+      (is (nil? (login-form/state-key @fe-state)) "Login form is cleared")
+      (is (= "alexander" (state/logged-in-user be-state nil)))
+      (be-action nil (login-form/on-click-logout nil fe-state) be-state)
+      (is (= nil (login-form/logged-in-key @fe-state)))
+      (is (= nil (state/logged-in-user be-state nil))))))
 
 (deftest registration-test
   (testing "successful registration"
@@ -71,9 +78,3 @@
       (is (not (contains? @client-state registration-form/state-key)))
       (is (= "alex" (state/logged-in-user be-state nil))))))
 
-(deftest ws-send-test
-  (testing "Checking sent msgs"
-    (let [msgs (atom [])]
-      (ws-send msgs "test1")
-      (ws-send msgs "test2")
-      (is (= ["test1" "test2"] @msgs)))))
